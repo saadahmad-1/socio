6af2451a36d16ed6b40967a6b2cf9924d086c984
Saad Ahmad <saadahmad.cs@gmail.com>
2024-06-25 15:17:53 +0500

    minor update and added vercel.json

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -6,9 +6,6 @@ import { createNextRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
-export const maxDuration = 60; // This function can run for a maximum of 5 seconds
-export const dynamic = 'force-dynamic';
-
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -34,9 +34,6 @@ type Event = {
   type: EventType;
 };
 
-export const maxDuration = 60; // This function can run for a maximum of 5 seconds
-export const dynamic = 'force-dynamic';
-
 export const POST = async (request: Request) => {
   const payload = await request.json();
   const header = headers();
