a1769144e6a97a607e9b9f18197643d217f2c4d7
Saad Ahmad <saadahmad.cs@gmail.com>
2024-06-25 14:23:46 +0500

    updated vercel time

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,9 +1,13 @@
 // Resource: https://docs.uploadthing.com/nextjs/appdir#create-a-nextjs-api-route-using-the-filerouter
 // Copy paste (be careful with imports)
 
+
 import { createNextRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
+
+export const maxDuration = 60; // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic';
 
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
